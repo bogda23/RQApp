06ab24baf105ec6d03f923e37374d206a9b5b6f6
bogdanivascu <ivascubogdan97@gmail.com>
2020-03-29 02:58:33 +0200

    Create CustomAnimation.java

---
+++ app/src/main/java/com/usv/rqapp/CustomAnimation.java
@@ -0,0 +1,8 @@
+package com.usv.rqapp;
+
+public class CustomAnimation {
+
+    public static int[] animation = {R.anim.enter_right_to_left, R.anim.exit_right_to_left,
+            R.anim.enter_left_to_right, R.anim.exit_left_to_right};
+    public static int[] animationBack = {R.anim.enter_left_to_right, R.anim.exit_left_to_right};
+}
