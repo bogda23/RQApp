162369d4daf3b34115c8b311aa65fe6219b39836
bogdanivascu <ivascubogdan97@gmail.com>
2020-03-29 02:58:22 +0200

    Update CONSTANTS.java

--- app/src/main/java/com/usv/rqapp/CONSTANTS.java
+++ app/src/main/java/com/usv/rqapp/CONSTANTS.java
@@ -1,8 +1,24 @@
 package com.usv.rqapp;
 
+import android.content.res.Resources;
+
+import com.google.android.gms.common.util.Strings;
+
 public final class CONSTANTS {
+
+
+
     public static final String BANNER_ID="ca-app-pub-3550303973503881/1149433866";
     public static final String BANNER_ID_SAMPLE="ca-app-pub-3940256099942544/6300978111";
     public static final String APPLICATION_ID="ca-app-pub-3550303973503881~3123972467";
     public static final String HUAWEI_ID="E57FF2B8557ED59E682F29B0AE32A805";
+
+    //Register with Google
+    public static final String RC_SIGN_IN="977131281111-5fufu7a8a12insv8t4rlhmk0d63cehsu.apps.googleusercontent.com";
+
+    //Network status
+    public static final String WIFI_ACTIVATED ="Wifi activat";
+    public static final String DATA_ACTIVATED ="Date mobile activate";
+    public static final String NO_INTERNET = "Nu ești conectat la internet!";
+
 }
