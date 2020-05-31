5b8b3a13b4f3f9a971ead6b212af055220ff0db8
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-05-31 12:49:00 +0300

    Update register

--- app/src/main/java/com/usv/rqapp/fragments/RegisterFragment.java
+++ app/src/main/java/com/usv/rqapp/fragments/RegisterFragment.java
@@ -1,25 +1,99 @@
 package com.usv.rqapp.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.usv.rqapp.CONSTANTS;
+import com.usv.rqapp.CustomAnimation;
 import com.usv.rqapp.R;
+import com.usv.rqapp.databinding.FragmentRegisterBinding;
+
+import java.util.concurrent.Executor;
 
 public class RegisterFragment extends Fragment {
+
+    private static final String TAG = "RegisterFragment";
+    private String userResponseToken;
+    private View registerView;
+    private FragmentRegisterBinding binding;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
+        registerView = binding.getRoot();
 
-        return view;
+        buttonHandler();
+
+        return registerView;
     }
+
+    private void loadReCaptcha() {
+        SafetyNet.getClient(getContext()).verifyWithRecaptcha(CONSTANTS.SITE_KEY)
+                .addOnSuccessListener((Executor) this,
+                        new OnSuccessListener<SafetyNetApi.RecaptchaTokenResponse>() {
+                            @Override
+                            public void onSuccess(SafetyNetApi.RecaptchaTokenResponse response) {
+                                // Indicates communication with reCAPTCHA service was
+                                // successful.
+                                userResponseToken = response.getTokenResult();
+                                if (!userResponseToken.isEmpty()) {
+                                    // Validate the user response token using the
+                                    // reCAPTCHA siteverify API.
+                                    //todo: We need retrofit
+                                }
+                            }
+                        })
+                .addOnFailureListener((Executor) this, new OnFailureListener() {
+
+
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (e instanceof ApiException) {
+                            // An error occurred when communicating with the
+                            // reCAPTCHA service. Refer to the status code to
+                            // handle the error appropriately.
+                            ApiException apiException = (ApiException) e;
+                            int statusCode = apiException.getStatusCode();
+                            Log.d(TAG, "Error: " + CommonStatusCodes
+                                    .getStatusCodeString(statusCode));
+                        } else {
+                            // A different, unknown type of error occurred.
+                            Log.d(TAG, "Error: " + e.getMessage());
+                        }
+                    }
+                });
+
+    }
+
+
+    private void buttonHandler() {
+        binding.tvRegister.setOnClickListener(v -> {
+            getFragmentManager().popBackStackImmediate();
+        });
+    }
+
 
     public static RegisterFragment newInstance() {
         RegisterFragment fragment = new RegisterFragment();
