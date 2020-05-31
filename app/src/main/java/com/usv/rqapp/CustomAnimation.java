1d7f7b2a02b9e28d2e62ef1aa405a7975662be15
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-05-31 12:44:36 +0300

    Add custom animations

--- app/src/main/java/com/usv/rqapp/CustomAnimation.java
+++ app/src/main/java/com/usv/rqapp/CustomAnimation.java
@@ -1,8 +1,23 @@
 package com.usv.rqapp;
 
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
 public class CustomAnimation {
+
+    private static Animation genericAnimation;
 
     public static int[] animation = {R.anim.enter_right_to_left, R.anim.exit_right_to_left,
             R.anim.enter_left_to_right, R.anim.exit_left_to_right};
-    public static int[] animationBack = {R.anim.enter_left_to_right, R.anim.exit_left_to_right};
+    public static int[] animationExit = {R.anim.enter_left_to_right, R.anim.exit_left_to_right};
+    public static int[] animationFadeExit = {R.anim.fade_in, R.anim.fade_out};
+
+    public static void applyAnimationTo(View view, int animationResource) {
+        genericAnimation = AnimationUtils.loadAnimation(view.getContext(), animationResource);
+        view.startAnimation(genericAnimation);
+
+    }
+
 }
+
