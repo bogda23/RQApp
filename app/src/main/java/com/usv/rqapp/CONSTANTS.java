4e0b4240bde682a77e3539a2790be91b36947d64
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-06-05 19:21:07 +0300

    Update CONSTANTS.java

--- app/src/main/java/com/usv/rqapp/CONSTANTS.java
+++ app/src/main/java/com/usv/rqapp/CONSTANTS.java
@@ -6,22 +6,29 @@ import com.google.android.gms.common.util.Strings;
 
 public final class CONSTANTS {
 
+    //Login
+    public static final String INVALIDE_EMAIL = "Email invalid";
+    public static final String MIN_SIX_CHARS_PASSWORD = "minim 6 caractere";
+    public static final String INVALIDE_PASSWORD = "Parola invalida";
+    public final static String SHOW = "SHOW";
+    public final static String HIDE = "HIDE";
 
 
-    public static final String BANNER_ID="ca-app-pub-3550303973503881/1149433866";
-    public static final String BANNER_ID_SAMPLE="ca-app-pub-3940256099942544/6300978111";
-    public static final String APPLICATION_ID="ca-app-pub-3550303973503881~3123972467";
-    public static final String HUAWEI_ID="E57FF2B8557ED59E682F29B0AE32A805";
+    //Ads
+    public static final String BANNER_ID = "ca-app-pub-3550303973503881/1149433866";
+    public static final String BANNER_ID_SAMPLE = "ca-app-pub-3940256099942544/6300978111";
+    public static final String APPLICATION_ID = "ca-app-pub-3550303973503881~3123972467";
+    public static final String HUAWEI_ID = "E57FF2B8557ED59E682F29B0AE32A805";
 
     //Register with Google
-    public static final String RC_SIGN_IN="977131281111-5fufu7a8a12insv8t4rlhmk0d63cehsu.apps.googleusercontent.com";
+    public static final String RC_SIGN_IN = "977131281111-5fufu7a8a12insv8t4rlhmk0d63cehsu.apps.googleusercontent.com";
 
     //Network status
-    public static final String WIFI_ACTIVATED ="Wifi activat";
-    public static final String DATA_ACTIVATED ="Date mobile activate";
+    public static final String WIFI_ACTIVATED = "Wifi activat";
+    public static final String DATA_ACTIVATED = "Date mobile activate";
     public static final String NO_INTERNET = "Nu ești conectat la internet!";
 
     //reCaptcha
-    public static final String SITE_KEY ="6Lf29eQUAAAAAM83w-hHl8B9sN4RFyP4ca6DX7Fm";
+    public static final String SITE_KEY = "6Lf29eQUAAAAAM83w-hHl8B9sN4RFyP4ca6DX7Fm";
 
 }
