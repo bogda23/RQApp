5b707179a6e174952119b01a8fb22d3deb9fb75d
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-06-11 16:17:09 +0300

    Create FragmentOpener.java

---
+++ app/src/main/java/com/usv/rqapp/controller/FragmentOpener.java
@@ -0,0 +1,17 @@
+package com.usv.rqapp.controller;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.usv.rqapp.CustomAnimation;
+import com.usv.rqapp.R;
+import com.usv.rqapp.fragments.MapsFragment;
+
+public class FragmentOpener {
+
+    public static void loadNextFragment(Fragment newFragment, FragmentManager fragmentManager) {
+        FragmentManager manager = fragmentManager;
+        manager.beginTransaction().setCustomAnimations(CustomAnimation.animation[0], CustomAnimation.animation[1],
+                CustomAnimation.animation[2], CustomAnimation.animation[3]).replace(R.id.fragment_frame, newFragment).commit();
+    }
+}
