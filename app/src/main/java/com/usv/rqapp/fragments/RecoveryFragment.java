1d210bf2a86c5b132ccb3f997bb60327e8715b50
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-06-11 16:16:52 +0300

    Add password recovery functionality

--- app/src/main/java/com/usv/rqapp/fragments/RecoveryFragment.java
+++ app/src/main/java/com/usv/rqapp/fragments/RecoveryFragment.java
@@ -1,26 +1,50 @@
 package com.usv.rqapp.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.usv.rqapp.CONSTANTS;
+import com.usv.rqapp.CustomAnimation;
 import com.usv.rqapp.R;
+import com.usv.rqapp.controller.Verifier;
 import com.usv.rqapp.databinding.FragmentRecoveryBinding;
 
 public class RecoveryFragment extends Fragment {
+    private static final String TAG = "RecoveryFragment";
     private View recoveryView;
     private FragmentRecoveryBinding binding;
+    private FragmentManager manager;
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         binding = null;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        manager = getFragmentManager();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
     }
 
     @Nullable
@@ -30,15 +54,59 @@ public class RecoveryFragment extends Fragment {
         recoveryView = binding.getRoot();
 
         backPRessHandler();
+        passwordRecoverHandler();
 
         return recoveryView;
+    }
+
+
+    private void passwordRecoverHandler() {
+        binding.btnRecovery.setOnClickListener(click -> recoverPassword());
+    }
+
+    private boolean validateEmailField() {
+        boolean isValid = false;
+        if (TextUtils.isEmpty(binding.edtEmailRecovery.getText().toString())) {
+            binding.edtEmailRecovery.setError(CONSTANTS.INVALIDE_EMAIL);
+            binding.edtEmailRecovery.requestFocus();
+        } else if (!Verifier.validEmail(binding.edtEmailRecovery.getText().toString())) {
+            binding.edtEmailRecovery.setError(CONSTANTS.INCORECT_EMAIL);
+            binding.edtEmailRecovery.requestFocus();
+        } else {
+
+            isValid = true;
+        }
+
+        return isValid;
+    }
+
+    private void recoverPassword() {
+        if (validateEmailField()) {
+            FirebaseAuth.getInstance().sendPasswordResetEmail(binding.edtEmailRecovery.getText().toString())
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                            binding.tvEmailTrimis.setVisibility(View.VISIBLE);
+                            binding.tvEmailTrimis.setTextColor(getResources().getColor(R.color.colorGreen));
+                            binding.tvEmailTrimis.setText(CONSTANTS.EMAIL_SENT_SUCCESS);
+                            Toast.makeText(getActivity(), CONSTANTS.EMAIL_SENT_SUCCESS,Toast.LENGTH_LONG).show();
+                            manager.beginTransaction().setCustomAnimations(CustomAnimation.animation[0], CustomAnimation.animation[1],
+                                    CustomAnimation.animation[2], CustomAnimation.animation[3]).replace(R.id.fragment_frame, LoginFragment.newInstance()).commit();
+                        } else {
+                            binding.tvEmailTrimis.setVisibility(View.VISIBLE);
+                            binding.tvEmailTrimis.setTextColor(getResources().getColor(R.color.colorRed));
+                            binding.tvEmailTrimis.setText(CONSTANTS.EMAIL_SENT_ERROR);
+                        }
+
+                    });
+        }
     }
 
     private void backPRessHandler() {
         binding.btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getFragmentManager().popBackStackImmediate();
+                manager.popBackStackImmediate();
 
             }
         });
