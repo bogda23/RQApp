910c0e69e3d2f22a72e8cd51d3ff61d7fe6825bb
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-06-11 16:08:47 +0300

    Create Forwarder.java

---
+++ app/src/main/java/com/usv/rqapp/controller/Forwarder.java
@@ -0,0 +1,20 @@
+package com.usv.rqapp.controller;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class Forwarder {
+
+    //TODO: SHARE INFORMATION TO MESSENGER, WHATSAPP ETC
+    public static void shareInformation(Context context) {
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("plain/text");
+        intent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{});
+        intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+
+        /* Send it off to the Activity-Chooser */
+        context.startActivity(Intent.createChooser(intent, "Send"));
+    }
+}
