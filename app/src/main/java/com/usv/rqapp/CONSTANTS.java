c016b040ea499bb2be579c3d2c7b1f68e3175f93
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-06-11 16:09:27 +0300

    Update CONSTANTS.java

--- app/src/main/java/com/usv/rqapp/CONSTANTS.java
+++ app/src/main/java/com/usv/rqapp/CONSTANTS.java
@@ -6,13 +6,18 @@ import com.google.android.gms.common.util.Strings;
 
 public final class CONSTANTS {
 
-    //Login
+    //Login email
+    public static final CharSequence EMPTY_EMAIL = "Introduce-ți un email";
     public static final String INVALIDE_EMAIL = "Email invalid";
+    public static final String INCORECT_EMAIL = "Email incorect";
     public static final String EMAIL_ALREADY_EXISTS = "Aveti deja cont cu acest email";
-    public static final String VERIFY_EMAIL = "Email-ul este corect?";
-    public static final String VERIFY_PASSWORD = "Parola este corectă?";
+    public static final String YOU_DONT_HAVE_ACCOUNT = "Nu aveți cont! Creați unul mai întai";
+    public static final String SURE_YOU_HAVE_ACCOUNT = "Esti sigur că ai cont?";
+    //Login pass
+    public static final CharSequence EMPTY_PASSWORD = "Introduce-ți o parolă";
+    public static final String VERIFY_PASSWORD = "Parola este incorectă";
     public static final String MIN_SIX_CHARS_PASSWORD = "minim 6 caractere";
-    public static final String INVALIDE_PASSWORD = "Parola invalida";
+    public static final String INVALIDE_PASSWORD = "Parolă invalidă";
     public final static String SHOW = "SHOW";
     public final static String HIDE = "HIDE";
 
@@ -32,6 +37,9 @@ public final class CONSTANTS {
     public static final String NO_INTERNET = "Nu ești conectat la internet!";
 
     //reCaptcha
-    public static final String SITE_KEY = "6Lf29eQUAAAAAM83w-hHl8B9sN4RFyP4ca6DX7Fm";
+    public static final String RECAPTCHA_SITE_KEY = "6Lf29eQUAAAAAM83w-hHl8B9sN4RFyP4ca6DX7Fm";
+
+    public static final String EMAIL_SENT_SUCCESS = "Verifică email-ul";
+    public static final String EMAIL_SENT_ERROR = "Email-ul nu a fost trimis";
 
 }
