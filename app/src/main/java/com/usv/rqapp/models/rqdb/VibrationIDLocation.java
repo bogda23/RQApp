9dae3588336c80e14a79fd218ea03aa9cdb4ed1b
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-06-21 19:32:12 +0300

    Add Vibration Service Model

---
+++ app/src/main/java/com/usv/rqapp/models/rqdb/VibrationIDLocation.java
@@ -0,0 +1,53 @@
+package com.usv.rqapp.models.rqdb;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class VibrationIDLocation implements Serializable {
+
+    @SerializedName("lat")
+    private Double latitude;
+
+    @SerializedName("lng")
+    private Double longitude;
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof VibrationIDLocation)) return false;
+        VibrationIDLocation that = (VibrationIDLocation) o;
+        return Objects.equals(getLatitude(), that.getLatitude()) &&
+                Objects.equals(getLongitude(), that.getLongitude());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLatitude(), getLongitude());
+    }
+
+    @Override
+    public String toString() {
+        return "VibrationIDLocation{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/usv/rqapp/models/rqdb/VibrationObject.java
@@ -0,0 +1,106 @@
+package com.usv.rqapp.models.rqdb;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+public class VibrationObject implements Serializable {
+
+    @SerializedName("_id")
+    private VibrationIDLocation vibrationID;
+
+    @SerializedName("vibration_value")
+    private Double vibrationValue;
+
+    @SerializedName("last_update")
+    private Timestamp lastUpdate;
+
+    @SerializedName("country_name")
+    private String countryName;
+
+    @SerializedName("iso_code")
+    private String isoCode;
+
+    @SerializedName("county_name")
+    private  String countyName;
+
+    public VibrationIDLocation getVibrationID() {
+        return vibrationID;
+    }
+
+    public void setVibrationID(VibrationIDLocation vibrationID) {
+        this.vibrationID = vibrationID;
+    }
+
+    public Double getVibrationValue() {
+        return vibrationValue;
+    }
+
+    public void setVibrationValue(Double vibrationValue) {
+        this.vibrationValue = vibrationValue;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getIsoCode() {
+        return isoCode;
+    }
+
+    public void setIsoCode(String isoCode) {
+        this.isoCode = isoCode;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof VibrationObject)) return false;
+        VibrationObject that = (VibrationObject) o;
+        return Objects.equals(getVibrationID(), that.getVibrationID()) &&
+                Objects.equals(getVibrationValue(), that.getVibrationValue()) &&
+                Objects.equals(getLastUpdate(), that.getLastUpdate()) &&
+                Objects.equals(getCountryName(), that.getCountryName()) &&
+                Objects.equals(getIsoCode(), that.getIsoCode()) &&
+                Objects.equals(getCountyName(), that.getCountyName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getVibrationID(), getVibrationValue(), getLastUpdate(), getCountryName(), getIsoCode(), getCountyName());
+    }
+
+    @Override
+    public String toString() {
+        return "VibrationObject{" +
+                "vibrationID=" + vibrationID +
+                ", vibrationValue=" + vibrationValue +
+                ", lastUpdate=" + lastUpdate +
+                ", countryName='" + countryName + '\'' +
+                ", isoCode='" + isoCode + '\'' +
+                ", countyName='" + countyName + '\'' +
+                '}';
+    }
+}
