cfaf43868fbd7362ca0e0c0adf82aaa9785453c7
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-06-22 18:45:00 +0300

    Add dateOfBirth real time update + change password through recovery

--- app/src/main/java/com/usv/rqapp/fragments/UserAccountFragment.java
+++ app/src/main/java/com/usv/rqapp/fragments/UserAccountFragment.java
@@ -1,12 +1,16 @@
 package com.usv.rqapp.fragments;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,18 +19,24 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.usv.rqapp.CONSTANTS;
 import com.usv.rqapp.CustomAnimation;
 import com.usv.rqapp.R;
+import com.usv.rqapp.controllers.DateHandler;
+import com.usv.rqapp.controllers.FirestoreController;
 import com.usv.rqapp.databinding.FragmentUserAccountBinding;
 import com.usv.rqapp.models.db.User;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserAccountFragment extends Fragment {
 
@@ -34,8 +44,20 @@ public class UserAccountFragment extends Fragment {
     private View accountView;
     private FragmentUserAccountBinding binding;
     private FirebaseAuth auth;
-    private FirebaseUser user;
+    private FirebaseUser firebaseUser;
     private FirebaseFirestore db;
+    private FirestoreController firestoreController;
+
+    private FragmentManager manager;
+
+    private DatePickerDialog.OnDateSetListener dateSetListener;
+    private String userID;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        manager = getFragmentManager();
+    }
 
     @Nullable
     @Override
@@ -45,20 +67,117 @@ public class UserAccountFragment extends Fragment {
         accountView = binding.getRoot();
         initFirebase();
         initFirestoreDatabase();
+        loadAccountData();
+        loadDateOfBirthFromFirebase();
 
+        handleDatePicker();
+        handleChangePassword();
         handleDeleteAccount();
         signOutHandler();
 
         return accountView;
     }
 
+    public void loadDateOfBirthFromFirebase() {
+        if (firebaseUser.getUid() != null) {
+            DocumentReference documentReference = db.collection(User.UTILIZATORI).document(firebaseUser.getUid());
+            documentReference.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (documentSnapshot != null && documentSnapshot.exists() && documentSnapshot.getString(User.DATA_NASTERE) != null) {
+                        Log.e(TAG + " Data nastere exista", documentSnapshot.getId());
+                        binding.btnDatePicker.setText(documentSnapshot.getString(User.DATA_NASTERE));
+
+                    } else {
+                        Log.e(TAG + " Data nastere nu exista", "Data Empty");
+
+                    }
+                }
+            })
+                    .addOnFailureListener(e -> {
+                        Log.e(TAG + "Data nastere eroare: ", e.getMessage());
+
+                    });
+        }
+    }
+
+    private void handleDatePicker() {
+        binding.btnDatePicker.setOnClickListener(click -> {
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog dialog = new DatePickerDialog(getActivity(), android.R.style.Theme_Holo_Light_Dialog_MinWidth, dateSetListener, year, month, day);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String date = year + "-" + (month + 1) + "-" + dayOfMonth;
+                binding.btnDatePicker.setText(date);
+                User user = new User(firebaseUser.getUid(), date, true);
+                firestoreController.updateUserDateOdBirthInFirestore(user);
+
+            }
+        };
+    }
+
+
+    private void handleChangePassword() {
+        binding.tvChangePassword.setOnClickListener(v -> {
+            binding.progressBarHolder.setVisibility(View.VISIBLE);
+            AlertDialog.Builder dialog = new AlertDialog.Builder(getContext());
+            dialog.setTitle(CONSTANTS.CHANGE_PASSWORD);
+            dialog.setMessage(CONSTANTS.CHANGE_PASSWORD_MESSAGE);
+            dialog.setPositiveButton(getString(R.string.schimba_parola), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    AuthUI.getInstance().signOut(getActivity()).addOnCompleteListener(task -> {
+                        try {
+                            finalize();
+                            Toast.makeText(getContext(), "Sign out", Toast.LENGTH_LONG).show();
+                            FirebaseAuth.getInstance().signOut();
+                            goToRecoveryFragment();
+                        } catch (Throwable throwable) {
+                            throwable.printStackTrace();
+                        }
+                    });
+                }
+            });
+            dialog.setNegativeButton(getString(R.string.anuleaza), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    binding.progressBarHolder.setVisibility(View.GONE);
+                    dialog.dismiss();
+                }
+            });
+
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+
+        });
+    }
+
+    private void goToRecoveryFragment() {
+        manager.beginTransaction().replace(R.id.fragment_frame, RecoveryFragment.newInstance()).commit();
+    }
+
+    private void loadAccountData() {
+        binding.txtAccountName.setText(firebaseUser.getDisplayName());
+        binding.edtAccountEmail.setText(firebaseUser.getEmail());
+    }
+
     private void initFirestoreDatabase() {
         db = FirebaseFirestore.getInstance();
+        firestoreController = new FirestoreController();
     }
 
     private void initFirebase() {
         auth = FirebaseAuth.getInstance();
-        user = auth.getCurrentUser();
+        firebaseUser = auth.getCurrentUser();
     }
 
     private void handleDeleteAccount() {
@@ -70,7 +189,7 @@ public class UserAccountFragment extends Fragment {
             dialog.setPositiveButton(getString(R.string.sterge_contul), new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    user.delete().addOnCompleteListener(task -> {
+                    firebaseUser.delete().addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             deleteAccount();
                         } else {
@@ -96,7 +215,7 @@ public class UserAccountFragment extends Fragment {
     }
 
     private void deleteAccount() {
-        db.collection(User.UTILIZATORI).document(user.getUid()).delete().addOnCompleteListener(task -> {
+        db.collection(User.UTILIZATORI).document(firebaseUser.getUid()).delete().addOnCompleteListener(task -> {
             binding.progressBarHolder.setVisibility(View.GONE);
             if (task.isSuccessful()) {
                 Toast.makeText(getContext(), CONSTANTS.ACCOUNT_DELETED, Toast.LENGTH_LONG).show();
@@ -119,6 +238,7 @@ public class UserAccountFragment extends Fragment {
     private void signOutHandler() {
         binding.signOut.setOnClickListener(v -> {
             signOut();
+
         });
     }
 
