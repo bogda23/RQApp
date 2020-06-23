667d8335779685be2d14d35fcb30ad902ca6b00d
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-06-23 21:58:49 +0300

    Update FragmentOpener.java

--- app/src/main/java/com/usv/rqapp/controllers/FragmentOpener.java
+++ app/src/main/java/com/usv/rqapp/controllers/FragmentOpener.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentManager;
 
 import com.usv.rqapp.CustomAnimation;
 import com.usv.rqapp.R;
+import com.usv.rqapp.fragments.PermissionsFragment;
 
 public class FragmentOpener {
 
@@ -13,4 +14,11 @@ public class FragmentOpener {
         manager.beginTransaction().setCustomAnimations(CustomAnimation.animation[0], CustomAnimation.animation[1],
                 CustomAnimation.animation[2], CustomAnimation.animation[3]).replace(R.id.fragment_frame, newFragment).commit();
     }
+
+    public static void loadNextFragmentWithStack(Fragment newFragment, FragmentManager fragmentManager) {
+        FragmentManager manager = fragmentManager;
+        manager.beginTransaction().setCustomAnimations(CustomAnimation.animation[0], CustomAnimation.animation[1],
+                CustomAnimation.animation[2], CustomAnimation.animation[3]).replace(R.id.fragment_frame, newFragment).addToBackStack(null).commit();
+    }
+
 }
