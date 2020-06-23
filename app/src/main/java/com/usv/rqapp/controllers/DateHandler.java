14273e945aeb89b19c82b40b0e0ddf95d62fde29
Ivașcu Bogdan <ivascubogdan97@gmail.com>
2020-06-23 21:57:51 +0300

    Add time between now and then

--- app/src/main/java/com/usv/rqapp/controllers/DateHandler.java
+++ app/src/main/java/com/usv/rqapp/controllers/DateHandler.java
@@ -1,18 +1,87 @@
 package com.usv.rqapp.controllers;
 
 
-import java.sql.Timestamp;
+import com.google.firebase.Timestamp;
+
+import org.joda.time.DateTime;
+import org.joda.time.Hours;
+import org.joda.time.Minutes;
+import org.joda.time.Period;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateHandler {
 
-    public static Date getCurrentDate(){
-        long millis=System.currentTimeMillis();
-        java.sql.Date date=new java.sql.Date(millis);
+    public static Date getCurrentDate() {
+        long millis = System.currentTimeMillis();
+        java.sql.Date date = new java.sql.Date(millis);
         return date;
     }
-    public static Timestamp getCurrentTimestamp(){
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+    public static java.sql.Timestamp getCurrentTimestamp() {
+        java.sql.Timestamp timestamp = new java.sql.Timestamp(System.currentTimeMillis());
         return timestamp;
     }
+
+    public static Timestamp getCurrentFirestoreTimestamp() {
+        Timestamp timestamp = Timestamp.now();
+        return timestamp;
+    }
+
+    /**
+     * @param timestamp
+     * @return
+     */
+    public static String convertTimestampToString(Timestamp timestamp) {
+        final DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date data_stop = timestamp.toDate();
+        final String stop = formatter.format(data_stop);
+
+        return stop;
+    }
+
+
+    /**
+     * @param timestamp
+     * @return
+     */
+    public static String getTimeBetweenNowAndThen(Timestamp timestamp) {
+        final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX");
+
+        Date postDate = null;
+        Date currentDate = null;
+        int hoursBetween;
+        int minutesBetween;
+
+        try {
+
+            postDate = formatter.parse(formatter.format(timestamp.toDate()));
+            currentDate = formatter.parse(formatter.format(getCurrentDate()));
+
+            DateTime currentDateTime = new DateTime(postDate);
+            DateTime postDateTime = new DateTime(currentDate);
+
+            hoursBetween = (Hours.hoursBetween(currentDateTime, postDateTime).getHours() % 24);
+            minutesBetween = (Minutes.minutesBetween(currentDateTime, postDateTime).getMinutes() % 60);
+            if (hoursBetween != 0) {
+                return "acum " + hoursBetween + " de ore";
+            } else {
+                return "acum " + minutesBetween + " minute";
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return "eroare";
+    }
+
+
 }
